d7f628695e59b9cc32cda19550b038c1eab1f1b7
Dominick Peluso <peluso.dominick@gmail.com>
2015-09-09 21:03:37 -0400

    Add playlist

---
+++ js/Playlist.js
@@ -0,0 +1,25 @@
+define([''], function(){
+
+  var Playlist = function(){
+    // initialize
+    this.playlist = JSON.parse(sessionStorage.getItem('playlist')) || [];
+    this.listenAddSong();
+
+    this.updatePlaylist();
+
+  };
+
+  Playlist.prototype.addSong = function(song){
+    this.playlist.push(song);
+    this.updatePlaylist();
+  };
+  Playlist.prototype.removeSong = function(index){
+    this.playlist.splice(index, 1);
+    this.updatePlaylist();
+  };
+
+    return Playlist;
+
+});
+
+console.log('playlist.js');
